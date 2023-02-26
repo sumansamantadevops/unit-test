df5c70ffb4a7f45e248bf4bf47387ea10c80d832
Suman Samanta <105167794+sumansamanta376@users.noreply.github.com>
2023-02-26 21:30:44 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,11 +1,14 @@
 import unittest
 import requests
 
-class TestWebsite(unittest.TestCase):
-    def test_website_access(self):
-        url = "https://www.atg.world"
-        response = requests.get(url)
+class TestWebsiteAccessibility(unittest.TestCase):
+
+    def test_website_accessibility(self):
+        # Replace "https://www.example.com" with the URL of the website you want to test
+        response = requests.get("https://www.atg.world")
+        # Assert that the status code is 200, which indicates that the website is accessible
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
+
