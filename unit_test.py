35d494919dc14b3d5cc37bf9ec50c84365742e17
Suman Samanta <105167794+sumansamanta376@users.noreply.github.com>
2023-02-26 15:32:24 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -3,9 +3,9 @@ URL = "https://atg.world"
 try:
     response = requests.head(URL)
 except Exception as e:
-    print(f"Not OK: {str(e)}")
+    print(f"Fail: {str(e)}")
 else:
     if response.status_code == 200:
-        print("OK")
+        print("Pass")
     else:
         print(f"NOT OK: HTTP response code {response.status_code}")
